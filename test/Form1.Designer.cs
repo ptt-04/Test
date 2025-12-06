67876aacabb6cf4e5b56dad969492d34fed73711
ptt-04 <phantritai1552004@gmail.com>
2025-12-06 10:32:33 +0700

    matkhau

--- test/Form1.Designer.cs
+++ test/Form1.Designer.cs
@@ -30,6 +30,8 @@
 		{
 			this.label1 = new System.Windows.Forms.Label();
 			this.txtUsername = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtmatkhau = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -48,11 +50,29 @@
 			this.txtUsername.Size = new System.Drawing.Size(100, 22);
 			this.txtUsername.TabIndex = 1;
 			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(123, 77);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(61, 16);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Mật khẩu";
+			// 
+			// txtmatkhau
+			// 
+			this.txtmatkhau.Location = new System.Drawing.Point(240, 77);
+			this.txtmatkhau.Name = "txtmatkhau";
+			this.txtmatkhau.Size = new System.Drawing.Size(100, 22);
+			this.txtmatkhau.TabIndex = 3;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1138, 450);
+			this.Controls.Add(this.txtmatkhau);
+			this.Controls.Add(this.label2);
 			this.Controls.Add(this.txtUsername);
 			this.Controls.Add(this.label1);
 			this.Name = "Form1";
@@ -66,6 +86,8 @@
 
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.TextBox txtUsername;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtmatkhau;
 	}
 }
 
